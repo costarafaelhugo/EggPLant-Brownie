632d04e064081562469db2b5258b800a0b7a449a
hugogc <costarafaelhugo@gmail.com>
2023-04-04 16:52:39 -0300

    Initial Commit

---
+++ egg_plant_brownie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  egg_plant_brownie
+//
+//  Created by Hugo Rafael G. Gianpietro Costa on 04/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
