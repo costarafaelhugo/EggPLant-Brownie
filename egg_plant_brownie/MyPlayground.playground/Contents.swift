bb83636751c9862dbf8aae7fde956d7a048671c7
hugogc <costarafaelhugo@gmail.com>
2023-04-13 13:23:46 -0300

    usando o playground

---
+++ egg_plant_brownie/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let alimento = "Churros"
+
+let felicidade = 5
+
+let calorias = 79.5
+
+let vegetal = true
+
+//func alimentoConsumido(){
+//    print("O alimento consumido foi: \(alimento)")
+//}
+//
+//
+//alimentoConsumido()
+
+func alimentoConsumido(_ alimento:String, _ calorias:Double){
+    print("O ali,ento consumido foi: \(alimento) com \(calorias) ")
+}
+
+alimentoConsumido("churros", 12.0)
